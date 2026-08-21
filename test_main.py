6dc9a3035b31e157799531d9385ac0007152a3c2
Yue Li <yueli@Yues-MacBook-Pro.local>
2026-08-20 18:38:40 -0700

    Fix health endpoint test

--- test_main.py
+++ test_main.py
@@ -14,4 +14,4 @@ def test_health():
     response = client.get("/health")
 
     assert response.status_code == 200
-    assert response.json() == {"status": "broken"}
+    assert response.json() == {"status": "ok"}
